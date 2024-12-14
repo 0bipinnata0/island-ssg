a3c9706730b5f20e49895b36e59b3e8242c10e28
light <odinzero@outlook.com>
2024-12-14 20:31:05 +0800

    自动添加脚本

--- src/node/plugin-island/indexHtml.ts
+++ src/node/plugin-island/indexHtml.ts
@@ -1,11 +1,27 @@
 import { readFile } from "fs/promises";
 import { Plugin } from "vite";
-import { DEFAULT_HTML_PATH } from "../constants";
+import { CLIENT_ENTRY_PATH, DEFAULT_HTML_PATH } from "../constants";
 
 export function pluginIndexHtml(): Plugin {
   return {
     name: "island:index-html",
     apply: "serve",
+    transformIndexHtml(html) {
+      console.info("CLIENT_ENTRY_PATH", CLIENT_ENTRY_PATH);
+      return {
+        html,
+        tags: [
+          {
+            tag: "script",
+            attrs: {
+              type: "module",
+              src: `/@fs/${CLIENT_ENTRY_PATH}`,
+            },
+            injectTo: "body",
+          },
+        ],
+      };
+    },
     configureServer(server) {
       return () => {
         server.middlewares.use(async (req, res, next) => {
